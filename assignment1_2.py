831e6be867966aa9f4a43a954129f888445686a9
NighyRe <113569938+NighyRe@users.noreply.github.com>
2025-09-13 09:36:20 +0800

    Add files via upload

---
+++ assignment1_2.py
@@ -0,0 +1,20 @@
+import turtle
+import colorsys
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+s.bgcolor('black')
+
+t.speed(0)
+n = 36
+h = 0
+
+for i in range(200):
+  c = colorsys.hsv_to_rgb(h, 1, 0.9)
+  h += 1 / n
+  t.color(c)
+  t.left(170)
+  for j in range(5):
+    t.forward(210)
+    t.left(200)
